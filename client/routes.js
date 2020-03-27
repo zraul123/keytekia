0b57b2396c50844f63083464779ac5628052f6c9
zraul <zavaczki.raul@gmail.com>
2020-03-27 20:59:04 +0200

    remove import deck route

--- client/routes.js
+++ client/routes.js
@@ -7,7 +7,6 @@ import Register from './pages/Register';
 import Lobby from './pages/Lobby';
 import Decks from './pages/Decks';
 import Matches from './pages/Matches';
-import ImportDeck from './Components/Decks/ImportDeck';
 import HowToPlay from './pages/HowToPlay';
 import About from './pages/About';
 import Security from './pages/Security.jsx';
@@ -34,7 +33,6 @@ const routes = [
     { path: '/activation', action: context => <Activation key='activation' id={ context.params.id } token={ context.params.token } /> },
     { path: '/blocklist', action: () => <BlockList key='blocklist' /> },
     { path: '/decks', action: () => <Decks key='decks' /> },
-    { path: '/decks/import', action: () => <ImportDeck key='importDecks' /> },
     { path: '/decks/build/:id?', action: context => <Deckbuilder id={ context.params.id } key='deckbuilder' /> },
     { path: '/forgot', action: () => <ForgotPassword key='forgotpassword' /> },
     { path: '/how-to-play', action: () => <HowToPlay key='howtoplay' /> },
